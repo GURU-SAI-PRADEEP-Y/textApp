e9c0f20e63dfef72379c3164bd487952536de0c7
Guru sai pradeep <gurusaipradeepy@gmail.com>
2025-03-24 03:00:00 +0530

    I have learned about react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import './App.css'
 import Navbar from './components/Navbar'
 import TextForm from './components/TextForm'
-
+import About from '../src/components/About'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
-      <Navbar title="textApp" />
-      <TextForm placeholder="Write some text here" />
+      <Router>
+        <div>
+          <Navbar title="textApp" />
+          <Routes>
+            <Route path="/" element={<TextForm placeholder="Write some text here" />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   )
 }
